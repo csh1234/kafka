56372ac500b7face85510b58d422ed84d780b1f8
csh1234 <1397617269@qq.com>
2021-01-25 22:53:27 +0800

    #dev分支代码20211.15#

--- src/main/java/com/csh/kafa/TestJava.java
+++ src/main/java/com/csh/kafa/TestJava.java
@@ -4,5 +4,6 @@ public class TestJava {
     public static void main(String[] args) {
         System.out.println("测试类");
         System.out.println("202115.带点啥");
+        System.out.println("dev分支的");
     }
 }
