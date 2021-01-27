31dc7f93705f7dba64b7ad96e9dbb10711e5a837
csh1234 <1397617269@qq.com>
2021-01-27 20:14:30 +0800

    #dev分支开发#

--- src/main/java/com/csh/kafa/TestJava.java
+++ src/main/java/com/csh/kafa/TestJava.java
@@ -5,5 +5,7 @@ public class TestJava {
         System.out.println("测试类");
         System.out.println("202115.带点啥");
         System.out.println("dev分支的");
+        System.out.println("master20201.15.23:00");
+        System.out.println("dev20210127");
     }
 }
